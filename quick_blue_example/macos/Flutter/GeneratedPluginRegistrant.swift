fa3104127ae8e374e4d6409ac14c9ac6466f851d
wangkun <wangkun@woodemi.com>
2020-09-28 21:50:52 +0800

    Fix quick_blue_macos miscellaneous
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- quick_blue_example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ quick_blue_example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,5 +8,5 @@ import Foundation
 import quick_blue_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  QuickBluePlugin.register(with: registry.registrar(forPlugin: "QuickBluePlugin"))
+  QuickBlueMacosPlugin.register(with: registry.registrar(forPlugin: "QuickBlueMacosPlugin"))
 }
