c31ab377fc40adb08adc0b1d7d516e6a51c84f40
wangkun <wangkun@woodemi.com>
2020-09-28 16:32:32 +0800

    Refactor implementation to QuickBluePlatform
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- quick_blue/lib/quick_blue.dart
+++ quick_blue/lib/quick_blue.dart
@@ -1,14 +1,8 @@
-
 import 'dart:async';
 
-import 'package:flutter/services.dart';
+import 'package:quick_blue_platform_interface/quick_blue_platform_interface.dart';
 
 class QuickBlue {
-  static const MethodChannel _channel =
-      const MethodChannel('quick_blue');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  static Future<String> get platformVersion =>
+      QuickBluePlatform.instance.platformVersion;
 }
--- quick_blue_platform_interface/lib/quick_blue_platform_interface.dart
+++ quick_blue_platform_interface/lib/quick_blue_platform_interface.dart
@@ -1,7 +1,22 @@
 library quick_blue_platform_interface;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+import 'method_channel_quick_blue.dart';
+
+abstract class QuickBluePlatform extends PlatformInterface {
+  QuickBluePlatform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static QuickBluePlatform _instance = MethodChannelQuickBlue();
+
+  static QuickBluePlatform get instance => _instance;
+
+  static set instance(QuickBluePlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+
+  Future<String> get platformVersion;
 }
