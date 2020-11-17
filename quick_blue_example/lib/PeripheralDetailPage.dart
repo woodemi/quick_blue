a2165c634a35c1f6096af17ebfc02cd7a524292a
wangkun <wangkun.sunbreak@bytedance.com>
2020-11-18 06:34:48 +0800

    Add connect & disconnect to QuickBlue
    Signed-off-by: wangkun <wangkun.sunbreak@bytedance.com>

---
+++ quick_blue_example/lib/PeripheralDetailPage.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quick_blue/quick_blue.dart';
+
+class PeripheralDetailPage extends StatefulWidget {
+  final String deviceId;
+
+  PeripheralDetailPage(this.deviceId);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PeripheralDetailPageState();
+  }
+}
+
+class _PeripheralDetailPageState extends State<PeripheralDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PeripheralDetailPage'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('connect'),
+                onPressed: () {
+                  QuickBlue.connect(widget.deviceId);
+                },
+              ),
+              RaisedButton(
+                child: Text('disconnect'),
+                onPressed: () {
+                  QuickBlue.disconnect(widget.deviceId);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
