3006b144990b55c0c194eecd26bb3ce5b9d63122
unknown <rohit@androcid.co>
2022-03-10 09:14:39 -0800

    web_fix 2

--- quick_blue_web/lib/web.dart
+++ quick_blue_web/lib/web.dart
@@ -1,5 +1,4 @@
 library quick_blue_web;
 
-
 export 'src/quick_blue_web_unsupported.dart'
-    if (dart.library.html) 'src/flutter_web_bluetooth_web.dart';
+    if (dart.library.html) 'src/quick_blue_web.dart';
