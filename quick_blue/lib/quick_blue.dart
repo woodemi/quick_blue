5d7858e9ba1637f5f66681274d9f9738df2296af
wangkun <wangkun@woodemi.com>
2022-03-18 19:10:25 +0800

    Use quick_blue_linux on Linux
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- quick_blue/lib/quick_blue.dart
+++ quick_blue/lib/quick_blue.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:quick_blue_linux/quick_blue_linux.dart';
 import 'package:quick_blue_platform_interface/quick_blue_platform_interface.dart';
 
 import 'models.dart';
@@ -9,47 +11,68 @@ export 'models.dart';
 
 export 'package:quick_blue_platform_interface/models.dart';
 
+bool _manualDartRegistrationNeeded = true;
+
+QuickBluePlatform get _platform {
+  // This is to manually endorse Dart implementations until automatic
+  // registration of Dart plugins is implemented. For details see
+  // https://github.com/flutter/flutter/issues/52267.
+  if (_manualDartRegistrationNeeded) {
+    // Only do the initial registration if it hasn't already been overridden
+    // with a non-default instance.
+    if (Platform.isAndroid || Platform.isIOS
+      || Platform.isWindows || Platform.isMacOS) {
+      QuickBluePlatform.instance = MethodChannelQuickBlue();
+    } else if (Platform.isLinux) {
+      QuickBluePlatform.instance = QuickBlueLinux();
+    }
+    _manualDartRegistrationNeeded = false;
+  }
+
+  return QuickBluePlatform.instance;
+}
+
 class QuickBlue {
-  static Future<bool> isBluetoothAvailable() => QuickBluePlatform.instance.isBluetoothAvailable();
+  static Future<bool> isBluetoothAvailable() => _platform.isBluetoothAvailable();
 
-  static void startScan() => QuickBluePlatform.instance.startScan();
+  static void startScan() => _platform.startScan();
 
-  static void stopScan() => QuickBluePlatform.instance.stopScan();
+  static void stopScan() => _platform.stopScan();
 
   static Stream<BlueScanResult> get scanResultStream {
-    return QuickBluePlatform.instance.scanResultStream
+    return _platform.scanResultStream
       .map((item) => BlueScanResult.fromMap(item));
   }
 
-  static void connect(String deviceId) => QuickBluePlatform.instance.connect(deviceId);
+  static void connect(String deviceId) => _platform.connect(deviceId);
 
-  static void disconnect(String deviceId) => QuickBluePlatform.instance.disconnect(deviceId);
+  static void disconnect(String deviceId) => _platform.disconnect(deviceId);
 
   static void setConnectionHandler(OnConnectionChanged? onConnectionChanged) {
-    QuickBluePlatform.instance.onConnectionChanged = onConnectionChanged;
+    _platform.onConnectionChanged = onConnectionChanged;
   }
 
-  static void discoverServices(String deviceId) => QuickBluePlatform.instance.discoverServices(deviceId);
+  static void discoverServices(String deviceId) => _platform.discoverServices(deviceId);
 
   static void setServiceHandler(OnServiceDiscovered? onServiceDiscovered) {
-    QuickBluePlatform.instance.onServiceDiscovered = onServiceDiscovered;
+    _platform.onServiceDiscovered = onServiceDiscovered;
   }
 
   static Future<void> setNotifiable(String deviceId, String service, String characteristic, BleInputProperty bleInputProperty) {
-    return QuickBluePlatform.instance.setNotifiable(deviceId, service, characteristic, bleInputProperty);
+    return _platform.setNotifiable(deviceId, service, characteristic, bleInputProperty);
   }
 
   static void setValueHandler(OnValueChanged? onValueChanged) {
-    QuickBluePlatform.instance.onValueChanged = onValueChanged;
+    _platform.onValueChanged = onValueChanged;
   }
 
   static Future<void> readValue(String deviceId, String service, String characteristic) {
-    return QuickBluePlatform.instance.readValue(deviceId, service, characteristic);
+    return _platform.readValue(deviceId, service, characteristic);
   }
 
   static Future<void> writeValue(String deviceId, String service, String characteristic, Uint8List value, BleOutputProperty bleOutputProperty) {
-    return QuickBluePlatform.instance.writeValue(deviceId, service, characteristic, value, bleOutputProperty);
+    return _platform.writeValue(deviceId, service, characteristic, value, bleOutputProperty);
   }
 
-  static Future<int> requestMtu(String deviceId, int expectedMtu) => QuickBluePlatform.instance.requestMtu(deviceId, expectedMtu);
+  static Future<int> requestMtu(String deviceId, int expectedMtu) => _platform.requestMtu(deviceId, expectedMtu);
 }
