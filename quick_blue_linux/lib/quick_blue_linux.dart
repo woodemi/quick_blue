136bc5300442ff5e11bc86984501a42e00c33f1a
wangkun <wangkun@woodemi.com>
2022-03-21 05:54:35 +0800

    Rearrange QuickBlueLinux
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- quick_blue_linux/lib/quick_blue_linux.dart
+++ quick_blue_linux/lib/quick_blue_linux.dart
@@ -4,6 +4,28 @@ import 'package:quick_blue_platform_interface/quick_blue_platform_interface.dart
 
 class QuickBlueLinux extends QuickBluePlatform {
   @override
+  Future<bool> isBluetoothAvailable() {
+    // TODO: implement isBluetoothAvailable
+    throw UnimplementedError();
+  }
+
+  @override
+  void startScan() {
+    // TODO: implement startScan
+    throw UnimplementedError();
+  }
+
+  @override
+  void stopScan() {
+    // TODO: implement stopScan
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement scanResultStream
+  Stream get scanResultStream => throw UnimplementedError();
+
+  @override
   void connect(String deviceId) {
     // TODO: implement connect
     throw UnimplementedError();
@@ -22,8 +44,8 @@ class QuickBlueLinux extends QuickBluePlatform {
   }
 
   @override
-  Future<bool> isBluetoothAvailable() {
-    // TODO: implement isBluetoothAvailable
+  Future<void> setNotifiable(String deviceId, String service, String characteristic, BleInputProperty bleInputProperty) {
+    // TODO: implement setNotifiable
     throw UnimplementedError();
   }
 
@@ -34,36 +56,14 @@ class QuickBlueLinux extends QuickBluePlatform {
   }
 
   @override
-  Future<int> requestMtu(String deviceId, int expectedMtu) {
-    // TODO: implement requestMtu
-    throw UnimplementedError();
-  }
-
-  @override
-  // TODO: implement scanResultStream
-  Stream get scanResultStream => throw UnimplementedError();
-
-  @override
-  Future<void> setNotifiable(String deviceId, String service, String characteristic, BleInputProperty bleInputProperty) {
-    // TODO: implement setNotifiable
-    throw UnimplementedError();
-  }
-
-  @override
-  void startScan() {
-    // TODO: implement startScan
-    throw UnimplementedError();
-  }
-
-  @override
-  void stopScan() {
-    // TODO: implement stopScan
-    throw UnimplementedError();
-  }
-
-  @override
   Future<void> writeValue(String deviceId, String service, String characteristic, Uint8List value, BleOutputProperty bleOutputProperty) {
     // TODO: implement writeValue
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> requestMtu(String deviceId, int expectedMtu) {
+    // TODO: implement requestMtu
     throw UnimplementedError();
   }
 }
