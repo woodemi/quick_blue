b839035c9c4f63d8192f772a919883cccc8d1be8
Arwed Masch <arwed.masch@gmail.com>
2023-06-19 13:15:49 +0200

    Remove unnecessary debug log

--- quick_blue/ios/Classes/SwiftQuickBluePlugin.swift
+++ quick_blue/ios/Classes/SwiftQuickBluePlugin.swift
@@ -239,7 +239,7 @@ extension SwiftQuickBluePlugin: CBPeripheralDelegate {
   }
     
   public func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-    print("peripheral:didUpdateValueForForCharacteristic \(characteristic.uuid) \(characteristic.value as! NSData) error: \(error)")
+    // print("peripheral:didUpdateValueForForCharacteristic \(characteristic.uuid) \(characteristic.value as! NSData) error: \(error)")
     self.messageConnector.sendMessage([
       "deviceId": peripheral.uuid.uuidString,
       "characteristicValue": [
